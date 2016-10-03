119526bd3d7872d15b9dd1dcd3b4bba348bca292
TJ van Os <tjos@student.avans.nl>
2016-10-03 20:20:06 +0200

    init

--- DPA_Musicsheets/MidiHandler.cs
+++ DPA_Musicsheets/MidiHandler.cs
@@ -11,6 +11,8 @@ namespace DPA_Musicsheets
 
         internal static void getStuff(IEnumerable<MidiTrack> enumerable)
         {
+            string[] pitches = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
             //convert into song
             Song song = new Song();
 
@@ -28,15 +30,29 @@ namespace DPA_Musicsheets
             song.TimeSignature = new int[2] { Int32.Parse(result[1].Trim()), Int32.Parse(result[2].Trim()) };
             song.tempo = Int32.Parse(info.Messages[4].ToString().Split(':')[1]);
 
-            for (int i = 1; i < music.Messages.Count(); i += 2)//loop reading the notes, in pairs of 2 --> note on - note off | first and last are no notes
+            for (int i = 1; i < music.Messages.Count()-1; i ++)//loop reading the notes, in pairs of 2 --> note on - note off | first and last are no notes
             {
-                song.notes.Add(new DeezNuts());
-                // delen door 12 naar beneden afronden is octaaf
-                // modulo 12 --- 77 %12
+                DeezNuts note = new DeezNuts();
+                /*note.setOctave(67);*/               // delen door 12 naar beneden afronden is octaaf
+                                                // modulo 12 --- 77 %12
                 // list maken van noten
+                song.notes.Add(note);
+
 
             }
 
         }
+
+        public void addName(string name)
+        {
+
+        }
+
+        public void addnote(note)
+        {
+            
+        }
+
+
     }
 }
