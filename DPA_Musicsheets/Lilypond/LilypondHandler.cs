e029c6f04b2889487614ab3f7d225bffaf2580df
Tijn Kanters <trkanter@avans.nl>
2016-10-13 21:55:55 +0200

    Lilypond drawing working

--- DPA_Musicsheets/Lilypond/LilypondHandler.cs
+++ DPA_Musicsheets/Lilypond/LilypondHandler.cs
@@ -10,30 +10,30 @@ namespace DPA_Musicsheets
     {
         public Song song = new Song();
 
-        public void setTimeSignature(String signature)
+        public void setTimeSignature(String signatureString)
         {
-            var numbers = signature.Split('/');
+            var numbers = signatureString.Split('/');
             song.TimeSignature = new int[] { Int32.Parse(numbers[0]), Int32.Parse(numbers[1]) };
         }
 
-        internal void setClef(String clef)
+        internal void setClef(String clefString)
         {
             throw new NotImplementedException();
         }
 
-        internal void setTempo(String tempo)
+        internal void setTempo(String tempoString)
         {
-            var numbers = tempo.Split('=');
+            var numbers = tempoString.Split('=');
             song.tempo = Int32.Parse(numbers[1]);
         }
 
-        internal int readRelativeString(String relative)
+        internal int readRelativeString(String relativeString)
         {
-            if ((relative.Split('\'').Length - 1) == 1)
+            if ((relativeString.Split('\'').Length - 1) == 1)
             {
                 return 1;
             }
-            else if ((relative.Split(',').Length - 1) == 1)
+            else if ((relativeString.Split(',').Length - 1) == 1)
             {
                 return -1;
             }
@@ -42,9 +42,87 @@ namespace DPA_Musicsheets
                 return 0;
         }
 
-        internal void createNote(String note)
+        internal DeezNuts createNote(char currentPitch, int currentOctave, string pitch, int duration, bool point)
         {
+            //Create a new note and set some values
+            DeezNuts note = new DeezNuts();
 
+            Char mainPitch = Char.ToUpper(pitch[0]);
+            note.point = point;
+            note.duration = duration;
+            note.directionUp = true;
+
+            //Check if the note is a rest note
+            if (mainPitch != 'R')
+            {
+                //Check if the note has a special type IS or ES
+                if (pitch.Length > 1)
+                {
+                    String type = pitch.Substring(1, 2);
+                    if (type.Equals("IS"))
+                        note.type = 1;
+                    if (type.Equals("ES"))
+                        note.type = -1;
+                }
+                else
+                    note.type = 0;
+
+                Char lowest = 'A';
+                Char highest = 'G';
+
+                //Check how many steps are needed to find the closest pitch on the left side
+                int leftSteps = 0;
+                bool changeOctaveLeft = false;
+
+                for (char C = currentPitch; C > mainPitch; C--)
+                {
+                    leftSteps++;
+
+                    if (C == lowest)
+                    {
+                        changeOctaveLeft = true;
+                        C = highest;
+                    }
+                }
+
+                //Check how many steps are needed to find the closest pitch on the right side
+                int rightSteps = 0;
+                bool changeOctaveRight = false;
+
+                for (char C = currentPitch; C < mainPitch; C++)
+                {
+                    rightSteps++;
+
+                    if (C == highest)
+                    {
+                        changeOctaveRight = true;
+                        C = lowest;
+                    }
+                }
+
+                //Determine if either right or left way is shorter
+                if (leftSteps < rightSteps && changeOctaveLeft)
+                    note.octave = currentOctave - 1;
+
+                else if (rightSteps < leftSteps && changeOctaveRight)
+                    note.octave = currentOctave + 1;
+               
+                else
+                    note.octave = currentOctave;
+                
+
+                note.pitch = mainPitch;
+            }
+
+            //Else, note is a rest note
+            else
+            {
+                note.rest = true;
+            }
+
+
+
+            return note;
         }
     }
 }
--- DPA_Musicsheets/Lilypond/LilypondReader.cs
+++ DPA_Musicsheets/Lilypond/LilypondReader.cs
@@ -12,12 +12,12 @@ namespace DPA_Musicsheets
         internal static void OpenLilypond(string filePath, LilypondHandler handler)
         {
             String rawFile = System.IO.File.ReadAllText(filePath);
-            String[] LilypondParts = clearFile(rawFile);
+            String[] LilypondParts = cleanFile(rawFile);
 
             readLilypond(LilypondParts, handler);
            }
 
-        private static string[] clearFile(string rawFile)
+        private static string[] cleanFile(string rawFile)
         {
             //First of all remove unnecessary characters
             String newFile = rawFile
@@ -73,8 +73,50 @@ namespace DPA_Musicsheets
                         //sluit een nieuwe laag
                         break;
                     default:
-                        //Create new Note
-                        break;
+                        //Run a regex to split all values
+                        String pattern = @"(?<pitch>[a-z]{1,3})(?<sign>\W?)(?<duration>\d{1,2})(?<point>\W?)";
+                        String noteString = LilypondParts[i];
+                        String pitch;
+                        String sign;
+                        int duration;
+                        bool point;
+
+                        if (Regex.IsMatch(noteString, pattern))
+                        {
+                            Regex r = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                            pitch = r.Match(noteString).Result("${pitch}").ToUpper();
+                            sign = r.Match(noteString).Result("${sign}");
+                            duration = Int32.Parse(r.Match(noteString).Result("${duration}"));
+                            point = (r.Match(noteString).Result("${point}") == ".");
+
+                            if (sign != null)
+                            {
+                                if (sign.Equals("'"))
+                                {
+                                    currentOctave++;
+                                }
+                                if (sign.Equals(","))
+                                {
+                                    currentOctave--;
+                                }
+                            }
+
+                            //Create new Note
+                            DeezNuts note = handler.createNote(currentPitch, currentOctave, pitch, duration, point);
+                            handler.song.notes.Add(note);
+
+                            currentOctave = note.octave;
+
+                            if (!pitch.Equals("R"))
+                            {
+                                currentPitch = Char.ToUpper(pitch[0]);
+                            }
+
+                            break;
+                        }
+
+                        else
+                            break;
                 }
             }
         }
